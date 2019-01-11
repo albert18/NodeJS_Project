965d6d788946124523dca54ff479fd8649889252
albert18 <caps18@hotmail.com>
2019-01-11 07:23:59 -0500

    destructing

--- Section 2/2/play.js
+++ Section 2/2/play.js
@@ -6,7 +6,20 @@ const person = {
     }
 };
 
+const printName = ({ name }) => {
+    console.log(name);
+}
+
+printName(person)
+const {name, age} = person;
+console.log(name,age);
+
+// ---------------------------------------
+
 const hobbies = ["Sports", "Cooking"];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
+
 // for (let hobby in hobbies) {
 //     console.log(hobby);
 // }

97adc4725d39ff94e28a59e0b3a3966cb237592e
albert18 <caps18@hotmail.com>
2019-01-10 21:13:37 -0500

    function in nodejs

--- Section 2/1/play.js
+++ Section 2/1/play.js
@@ -1,10 +1,25 @@
-var name = 'Max';
-var age = 29;
-var hasHobbies = true;
+// const - never plan on changing like (let).
 
 
-function summarizeUser(userName, userAge, userHasHobby) {
-    return 'Name is ' + userName + 'age is ' + userAge + ' and the user has ' + userHasHobby
+const name = 'Max';
+let age = 29;
+const hasHobbies = true;
+
+
+age = 30;
+
+
+
+const summarizeUser = (userName, userAge, userHasHobby) => { //Create arrow function =>
+    return 'Name is ' + userName + ' and age is ' + userAge + ' and the user has some hobbies ' + userHasHobby
 };
 
-console.log(summarizeUser('Max', 27, 'Hobbies'));
+const add = (a,b) => a + b; // some shorcut of arrow function with multiple argument
+const addOne = a => a + 1; // some shorcut of arrow function with single argument
+const addRandom = () => 1 + 4; // some shorcut of arrow function with without giving any argument
+
+
+// console.log(addOne(2));
+// console.log(add(2,2))
+console.log(addRandom());
+console.log(summarizeUser(name, age, hasHobbies));

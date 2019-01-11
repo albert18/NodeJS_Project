4081bb25f2980f075b4abec987a50b3ac1c3091e
albert18 <caps18@hotmail.com>
2019-01-10 21:26:02 -0500

    adding some arrays

--- Section 2/2/play.js
+++ Section 2/2/play.js
@@ -10,7 +10,8 @@ const hobbies = ["Sports", "Cooking"];
 // for (let hobby in hobbies) {
 //     console.log(hobby);
 // }
-console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
+// console.log(hobbies);
+hobbies.push('Programming');
 console.log(hobbies);
-
 

d79a3a95ea114bffd872f5bfd1476e21e573b466
albert18 <caps18@hotmail.com>
2019-01-21 07:26:44 -0500

    Sectiob 5

---
+++ Section 5/first/app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware')
+    next(); // Allows to go to next in line
+});
+
+app.use((req, res, next) => {
+    console.log('Another middleware')
+    res.send('<h1>Test</h1>')
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);

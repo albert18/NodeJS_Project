f5a01811804a29ae835d687943a3c5f64db7bc35
albert18 <caps18@hotmail.com>
2019-03-01 07:22:17 -0500

    section 10.2

---
+++ Section 10/util/database.js
@@ -0,0 +1,19 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = callback => {
+    MongoClient.connect(
+        'mongodb+srv://johnDoe:Albert18@nodejscluster0-zlz10.mongodb.net/test?retryWrites=true'
+        )
+    .then(client => {
+        console.log('Connected!')
+        callback(callback);
+    })
+    .catch(err => {
+        console.log(err)
+    });
+}
+
+module.exports = mongoConnect;
+
+

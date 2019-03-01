ed4d8a503a5b083a5e8264c7915b27ca5ba243d3
albert18 <caps18@hotmail.com>
2019-03-01 07:32:14 -0500

    update section 10.2

--- Section 10.2/util/database.js
+++ Section 10.2/util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = callback => {
+    MongoClient.connect(
+        'mongodb+srv://johnDoe:Albert18@nodejscluster0-zlz10.mongodb.net/test?retryWrites=true'
+        )
+    .then(client => {
+        console.log('Connected!')
+        callback(callback);
+    })
+    .catch(err => {
+        console.log(err)
+    });
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
